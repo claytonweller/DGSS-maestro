28d0d9ec1020f0859dc91e3e962e2ed6dee3a907
Clayton <clayton.weller@gmail.com>
2020-05-18 21:56:44 -0600

    More robust logic for coxswain selection

--- app/actions/modules/boatrace/selectCoxswains.ts
+++ app/actions/modules/boatrace/selectCoxswains.ts
@@ -2,6 +2,8 @@ import { IActionElements } from '../../';
 import { IMessagePayload } from '../../messager';
 import { Team, Attendee, Connection } from '../../../../db';
 
+// TODO much of this could probably be repurposed and generalized for Team creation in other modules.
+
 export async function boatraceSelectCoxswains(actionElements: IActionElements) {
   const { body, event, messager, sockets } = actionElements;
   const { instance } = body.params.currentModule;
@@ -13,18 +15,16 @@ export async function boatraceSelectCoxswains(actionElements: IActionElements) {
     Connection.getBySource(['control', 'display'], performance_id),
   ]);
 
-  const boatsWithCoxswains = boats.map((b) => {
+  const { liveBoats, deadBoats } = removeDeadBoats(boats, 4);
+
+  const boatsWithCoxswains = liveBoats.map((b) => {
     const coxswain = selectCoxswain(b, attendees);
-    // What happens if no one is in the boat?
-    const boat = {
+    return {
       ...b,
       captain_name: coxswain.name,
       captain_aws_id: coxswain.aws_connection_id,
     };
-    return { coxswain, boat };
   });
-
-  // Need to updated boats with captains.
 
   const payload: IMessagePayload = {
     action: 'boatrace-coxswain-selected',
@@ -33,23 +33,50 @@ export async function boatraceSelectCoxswains(actionElements: IActionElements) {
     },
   };
 
-  const messages = boatsWithCoxswains.reduce((msgs, { boat, coxswain }) => {
-    const cxswnPayload: IMessagePayload = { ...payload, params: { coxswain, boat } };
-    const toCoxswain = messager.sendToIds({ event, payload: cxswnPayload, ids: [coxswain.aws_connection_id] }, sockets);
-
-    const crewPayload: IMessagePayload = { ...payload, params: { boat } };
-    const crewIds = boat.attendee_aws_ids.filter((id) => id !== coxswain.aws_connection_id);
-    const toCrew = messager.sendToIds({ event, payload: crewPayload, ids: crewIds }, sockets);
-
-    return [...msgs, toCoxswain, toCrew];
-  }, []);
-
+  const messages = createTeamMessages(payload, boatsWithCoxswains, messager, sockets);
   const ids = connections.map((c) => c.aws_connection_id);
   messages.push(messager.sendToIds({ ids, event, payload }, sockets));
+  const deadBoatsToSink = deadBoats.map((b) => Team.remove(b.id));
 
-  await Promise.all(messages);
-  // Need to send notification to Crowd/control
+  await Promise.all([...messages, ...deadBoatsToSink]);
 }
+
+///////
+
+const removeDeadBoats = (boats, minSize) => {
+  const { crewInNeedofBoat, liveBoats, deadBoats } = boats.reduce(
+    (split, b) => {
+      if (b.attendee_aws_ids.length >= minSize) {
+        split.liveBoats.push(b);
+      } else {
+        const crew = b.attendee_aws_ids.map((i) => {
+          return { attendee_aws_ids: i, deadBoatId: b.id };
+        });
+        split.crewInNeedofBoat = [...split.crewInNeedofBoat, ...crew];
+        split.deadBoats.push(b);
+      }
+      return split;
+    },
+    { crewInNeedofBoat: [], liveBoats: [], deadBoats: [] }
+  );
+
+  crewInNeedofBoat.forEach((c) => {
+    const closestBoat = liveBoats.reduce(
+      (closestBoat, b) => {
+        const isClosest = Math.abs(b.id - c.deadBoatId) < Math.abs(closestBoat.id - c.deadBoatId);
+        if (isClosest) {
+          console.log(b);
+          return b;
+        }
+        return closestBoat;
+      },
+      { id: -1 }
+    );
+    closestBoat.attendee_aws_ids.push(c.attendee_aws_ids);
+  });
+
+  return { liveBoats, deadBoats };
+};
 
 const selectCoxswain = (boat, attendees) => {
   const { coxswain } = boat.attendee_aws_ids.reduce(
@@ -72,4 +99,20 @@ const rankInterest = (string) => {
   if (string === 'Fine') return 2;
   if (string === "Don't even look at me") return 0;
   return 1;
+};
+
+const createTeamMessages = (payload, boatsWithCoxswains, messager, sockets) => {
+  return boatsWithCoxswains.reduce((msgs, boat) => {
+    const { captain_aws_id, captain_name, attendee_aws_ids } = boat;
+    const cxswnPayload: IMessagePayload = { ...payload, params: { boat, youAreCoxswain: true } };
+    const toCoxswain = messager.sendToIds({ event, payload: cxswnPayload, ids: [captain_aws_id] }, sockets);
+
+    const crewPayload: IMessagePayload = { ...payload, params: { boat, youAreCoxswain: false } };
+    const crewIds = boat.attendee_aws_ids.filter((id) => id !== captain_aws_id);
+    const toCrew = messager.sendToIds({ event, payload: crewPayload, ids: crewIds }, sockets);
+
+    const teamUpdate = Team.update(boat.id, { captain_name, captain_aws_id, attendee_aws_ids });
+
+    return [...msgs, toCoxswain, toCrew, teamUpdate];
+  }, []);
 };
