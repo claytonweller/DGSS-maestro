16c00a3a47d2846a0a483f2931e0493d3292a7c6
Clayton <clayton.weller@gmail.com>
2020-08-02 11:45:26 -0600

    Hopefully this fixes the connection overload issue

--- app/server.ts
+++ app/server.ts
@@ -1,64 +1,69 @@
-import * as WebSocket from 'ws'
-import { dbConnection } from '../db'
-import { manageEvent } from './actions'
-import { Connection } from '../db/connections'
-import { localMessager } from './localMessager'
-import * as AWS from 'aws-sdk'
-AWS.config.update({ region: 'us-east-1' })
+import * as WebSocket from 'ws';
+import { db } from '../db';
+import { manageEvent } from './actions';
+import { Connection } from '../db/connections';
+import { localMessager } from './localMessager';
+import * as AWS from 'aws-sdk';
+AWS.config.update({ region: 'us-east-1' });
 
-const wss = new WebSocket.Server({ port: 8080 })
-const clients = {}
-console.warn(`Websockets listening on wss://localhost:${wss.options.port}`)
-dbConnection()
+const wss = new WebSocket.Server({ port: 8080 });
+const clients = {};
+console.warn(`Websockets listening on wss://localhost:${wss.options.port}`);
+db.connect();
+console.warn('Connected to DB: ' + db.options.connectionString);
 
 wss.on('connection', async (ws, req) => {
-  console.log('CONNECT------\n')
-  const awsId = req.headers['sec-websocket-key']
-  clients[awsId] = ws
+  console.log('CONNECT------\n');
+  const awsId = req.headers['sec-websocket-key'];
+  clients[awsId] = ws;
   const createParams = {
     performance_id: 0,
     aws_connection_id: awsId,
     attendee_id: null,
-    source: null
-  }
-  const conn = await Connection.create(createParams)
-  console.log('CREATED: ', awsId)
-  ws.send(JSON.stringify({ action: 'local-server', params: conn }))
+    source: null,
+  };
+  const conn = await Connection.create(createParams);
+  console.log('CREATED: ', awsId);
+  ws.send(JSON.stringify({ action: 'local-server', params: conn }));
 
-  ws.on('message', async message => {
-
-    console.log(`Received message => ${message}`)
-    await manageEvent(formatAsAWSEvent(message, awsId), localMessager, { ws, wss, clients })
-  })
+  ws.on('message', async (message) => {
+    console.log(`Received message => ${message}`);
+    await manageEvent(formatAsAWSEvent(message, awsId), localMessager, { ws, wss, clients });
+  });
 
   ws.on('close', async () => {
-    await Connection.removeByConnectionId(awsId)
-    delete clients[awsId]
-    console.log('DELTED: ', awsId)
-    console.log('Clients remaining - ', Object.keys(clients).length)
-  })
-
-})
+    await Connection.removeByConnectionId(awsId);
+    delete clients[awsId];
+    console.log('DELTED: ', awsId);
+    console.log('Clients remaining - ', Object.keys(clients).length);
+  });
+});
 
 function formatAsAWSEvent(message, id) {
   return {
     requestContext: {
-      routeKey: "$default",
-      messageId: "KG99mey_IAMCK8w=",
-      eventType: "MESSAGE",
-      extendedRequestId: "KG99mGiKoAMF4pQ=",
-      requestTime: "28/Mar/2020:16:09:33 +0000",
-      messageDirection: "IN",
-      stage: "dev",
+      routeKey: '$default',
+      messageId: 'KG99mey_IAMCK8w=',
+      eventType: 'MESSAGE',
+      extendedRequestId: 'KG99mGiKoAMF4pQ=',
+      requestTime: '28/Mar/2020:16:09:33 +0000',
+      messageDirection: 'IN',
+      stage: 'dev',
       connectedAt: 1585411772322,
       requestTimeEpoch: 1585411773360,
-      identity: { cognitoIdentityPoolId: null, cognitoIdentityId: null, principalOrgId: null, cognitoAuthenticationType: null, userArn: null },
-      requestId: "KG99mGiKoAMF4pQ=",
-      domainName: "g298l0uqlc.execute-api.us-east-1.amazonaws.com",
+      identity: {
+        cognitoIdentityPoolId: null,
+        cognitoIdentityId: null,
+        principalOrgId: null,
+        cognitoAuthenticationType: null,
+        userArn: null,
+      },
+      requestId: 'KG99mGiKoAMF4pQ=',
+      domainName: 'g298l0uqlc.execute-api.us-east-1.amazonaws.com',
       connectionId: id,
-      apiId: "g298l0uqlc"
+      apiId: 'g298l0uqlc',
     },
     body: message,
-    isBase64Encoded: false
-  }
+    isBase64Encoded: false,
+  };
 }
